9bf63f870bf41de2aac7efe4ff883aec2922701e
maddec01 <pedrofacg@gmail.com>
2017-09-10 00:30:33 +0100

    added intro scene + fixed bugs

---
+++ Assets/Scripts/IntroManagement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroManagement : MonoBehaviour {
+	
+	public Button Continue;
+	public Animator IntroAnimation;
+
+	void Start () {
+		/*Scene loadLevel = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene (loadLevel.buildIndex);*/
+		Continue.gameObject.SetActive(false);
+		IntroAnimation = GetComponent<Animator> ();
+		IntroAnimation.PlayInFixedTime ("IntroCameraAnimation", -1, 0f);
+	}
+
+	public void ContinueButtonShow () {
+		Continue.gameObject.SetActive(true);
+	}
+
+	public void ContinueButtonPress () {
+		SceneManager.LoadScene("MainGame");
+	}
+}
