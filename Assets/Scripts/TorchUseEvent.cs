79b0b314ffed732472492df4489430bfebc19d7f
maddec01 <pedrofacg@gmail.com>
2017-09-09 22:28:58 +0100

    added animation to torch and event

---
+++ Assets/Scripts/TorchUseEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchUseEvent : MonoBehaviour {
+
+	public GameObject TorchLight;
+	public CheckPointControl CheckPoint;
+
+	public void LightTheTorch () {
+		GameMechanics.TourchLightStatus = true;
+		TorchLight.SetActive (true);
+	}
+}
